c44623cedcd9efc95ba416ebaaf67beeaa432ebe
skpatel0813 <160561616+skpatel0813@users.noreply.github.com>
2024-11-11 09:13:47 -0500

    Delete frontend/src directory

--- frontend/src/types/colorthief.d.ts
+++
@@ -1,8 +0,0 @@
-declare module 'colorthief' {
-    class ColorThief {
-      getColor(sourceImage: HTMLImageElement, quality?: number): [number, number, number];
-      getPalette(sourceImage: HTMLImageElement, colorCount?: number, quality?: number): [number, number, number][];
-    }
-    export default ColorThief;
-  }
-  
