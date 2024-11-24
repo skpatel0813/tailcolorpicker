97f73f2e370b009442bb30806c09f1f065cdc659
ethan-jones <egj77508@uga.edu>
2024-11-24 18:43:51 -0500

    Rename ColorWheel.tsx to ColorPicker.tsx and changed implementation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,7 +5,7 @@ import ImageColorPalette from './components/ImageColorPalette';
 import LoginModal from './components/LoginModal';
 import SignupModal from './components/SignupModal';
 import SavedPalettesModal from './components/SavedPalettesModal';
-import ColorWheel from './components/ColorWheel';
+import ColorPicker from './components/ColorPicker';
 
 const App: React.FC = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -84,7 +84,7 @@ const App: React.FC = () => {
         />
 
         <div>
-          <ColorWheel />
+          <ColorPicker />
         </div>
 
         <div className="flex items-center justify-center ">
